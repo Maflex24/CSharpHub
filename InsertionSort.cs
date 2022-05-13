ad723d93f391f1643aae62d1e7fe040a3a7aa52c
Maflex24 <maflex24@gmail.com>
2022-05-13 22:53:28 +0200

    4 Types of sorting implemnted with testing their time. Huge diffrence

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -10,13 +10,44 @@ namespace CSharpHub
     {
         public int[] Sort(int[] array)
         {
-            List<int> toSort = array.ToList();
+            List<int> unSorted = array.ToList();
             List<int> sorted = new List<int>();
 
-            for (int ToSortI = 0; ToSortI < array.Length; ToSortI++)
+            while (unSorted.Count > 0)
             {
-                for (int sortedI = 0; sortedI < sorted.Count; sortedI++)
+                int checkingNumber = unSorted[0];
+
+                if (sorted.Count == 0)
                 {
+                    sorted.Add(checkingNumber);
+                    unSorted.Remove(checkingNumber);
+                    continue;
+                }
+
+                for (int i = 0; i < sorted.Count; i++)
+                {
+                    if (checkingNumber >= sorted[i] && i < sorted.Count - 1) // can compare next value
+                    {
+                        if (checkingNumber <= sorted[i + 1])
+                        {
+                            sorted.Insert(i + 1, checkingNumber);
+                            unSorted.Remove(checkingNumber);
+                            break;
+                        }
+                    }
+
+                    if (checkingNumber >= sorted[i] && i == sorted.Count - 1) // it's last i of sorted list
+                    {
+                        sorted.Add(checkingNumber);
+                        unSorted.Remove(checkingNumber);
+                        break;
+                    }
+
+                    if (i == sorted.Count - 1) // if all sorted values was bigger than checking number
+                    {
+                        sorted.Insert(0, checkingNumber);
+                        unSorted.Remove(checkingNumber);
+                    }
 
                 }
             }
--- Program.cs
+++ Program.cs
@@ -2,56 +2,66 @@
 using CSharpHub;
 
 Random random = new Random();
+List<int> elementsQty = new List<int>() { 50, 250, 1000, 10000, 25000, 100000 };
 
-List<int> listOfNumbers = new List<int>();
-
-//just fill many numbers
-for (int i = 0; i < 10000; i++)
+foreach (var elementsValue in elementsQty)
 {
-    int randomNumber = random.Next(1, 50000);
-    listOfNumbers.Add(randomNumber);
+    List<int> listOfNumbers = new List<int>();
+
+    //just fill many numbers
+    for (int i = elementsValue; i > 0; i--)
+    {
+        listOfNumbers.Add(random.Next(1, elementsValue / 10));
+    }
+
+    int[] numbers = listOfNumbers.ToArray();
+    Console.WriteLine($"Testing sorting algorithms for {numbers.Length} elements");
+
+    Stopwatch quickWatch = Stopwatch.StartNew();
+    QuickSort quickSort = new QuickSort();
+    var quickSorted = quickSort.Sort(numbers);
+    quickWatch.Stop();
+    Console.WriteLine($"QuickSort takes: {quickWatch.ElapsedMilliseconds} ms");
+
+
+    Stopwatch selectionWatch = Stopwatch.StartNew();
+    SelectionSort selectionSort = new SelectionSort();
+    var selectionSorted = selectionSort.Sort(numbers);
+    selectionWatch.Stop();
+    Console.WriteLine($"SelectionSort takes: {selectionWatch.ElapsedMilliseconds} ms");
+
+
+    Stopwatch bubbleWatch = Stopwatch.StartNew();
+    BubbleSort bubbleSort = new BubbleSort();
+    var bubbleSorted = bubbleSort.Sort(numbers);
+    bubbleWatch.Stop();
+    Console.WriteLine($"BubbleSort takes: {bubbleWatch.ElapsedMilliseconds} ms");
+
+
+    Stopwatch insertionWatch = Stopwatch.StartNew();
+    InsertionSort insertionSort = new InsertionSort();
+    var insertionSorted = insertionSort.Sort(numbers);
+    insertionWatch.Stop();
+    Console.WriteLine($"InsertionSort takes: {insertionWatch.ElapsedMilliseconds} ms");
+
+    if (selectionSorted.Length != numbers.Length)
+        throw new Exception();
+
+    if (quickSorted.Length != numbers.Length)
+        throw new Exception();
+
+    if (bubbleSorted.Length != numbers.Length)
+        throw new Exception();
+
+    if (insertionSorted.Length != numbers.Length)
+        throw new Exception();
+
+    //Console.WriteLine(string.Join(", ", selectionSorted));
+    //Console.WriteLine(string.Join(", ", quickSorted));
+    //Console.WriteLine(string.Join(", ", bubbleSorted));
+    //Console.WriteLine(string.Join(", ", insertionSorted));
+    Console.WriteLine();
+
 }
 
-int[] numbers = listOfNumbers.ToArray();
 
-Stopwatch selectionWatch = Stopwatch.StartNew();
-SelectionSort selectionSort = new SelectionSort();
-var selectionSorted = selectionSort.Sort(numbers);
-selectionWatch.Stop();
-
-Stopwatch quickWatch = Stopwatch.StartNew();
-QuickSort quickSort = new QuickSort();
-var quickSorted = quickSort.Sort(numbers);
-quickWatch.Stop();
-
-Stopwatch bubbleWatch = Stopwatch.StartNew();
-BubbleSort bubbleSort = new BubbleSort();
-var bubbleSorted = bubbleSort.Sort(numbers);
-bubbleWatch.Stop();
-
-Stopwatch insertionWatch = Stopwatch.StartNew();
-InsertionSort insertionSort = new InsertionSort();
-var insertionSorted = insertionSort.Sort(numbers);
-insertionWatch.Stop();
-
-
-if (selectionSorted.Length != numbers.Length)
-    throw new Exception();
-
-if (quickSorted.Length != numbers.Length)
-    throw new Exception();
-
-if (bubbleSorted.Length != numbers.Length)
-    throw new Exception();
-
-if (insertionSorted.Length != numbers.Length)
-    throw new Exception();
-
-//Console.WriteLine(string.Join(", ", selectionSorted));
-//Console.WriteLine(string.Join(", ", quickSorted));
-//Console.WriteLine(string.Join(", ", bubbleSorted));
-
-Console.WriteLine($"Selection watch: {selectionWatch.ElapsedMilliseconds} ms");
-Console.WriteLine($"Quick watch: {quickWatch.ElapsedMilliseconds} ms");
-Console.WriteLine($"Bubble watch: {bubbleWatch.ElapsedMilliseconds} ms");
-Console.WriteLine($"Insertion watch: {insertionWatch.ElapsedMilliseconds} ms");

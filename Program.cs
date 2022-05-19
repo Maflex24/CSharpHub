4350a2a6a67ac3e93ee2a797aae84683a895bb0c
Maflex24 <maflex24@gmail.com>
2022-05-19 16:51:56 +0200

    Simple text logger

--- Program.cs
+++ Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿string projectName = "SimpleLogger";
+string loggersPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), projectName);
+
+string logFileName = "log.txt";
+string logFilePath = Path.Combine(loggersPath, logFileName);
+
+if (!Directory.Exists(loggersPath))
+    Directory.CreateDirectory(loggersPath);
+
+if (!File.Exists(logFilePath))
+    using (FileStream fs = new FileStream(logFilePath, FileMode.Create)) { }
+
+
+while (true)
+{
+    var logs = File.ReadAllLines(logFilePath).ToList();
+    foreach (var log in logs)
+    {
+        Console.WriteLine(log);
+    }
+
+    Console.Write("Add your log context: ");
+    var newLog = Console.ReadLine();
+
+    if (newLog != "" && newLog != "exit")
+    {
+        logs.Add($"{DateTime.Now} | {newLog}");
+        File.WriteAllLines(logFilePath, logs);
+        continue;
+    }
+
+    return;
+}
+
+

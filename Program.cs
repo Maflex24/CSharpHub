b50500f2acf8ddb1893738c699048d07629fb43c
Maflex24 <maflex24@gmail.com>
2022-06-02 16:05:42 +0200

    AsyncCoffee

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using CSharpHub;
+
+async Task FillWaterAsync()
+{
+    Console.WriteLine("Filling water to kettle");
+    await WaitingInfo.WaitDots(6, "fill the water");
+    Console.WriteLine("Kettle is full of water");
+}
+
+async Task BoilWaterAsync()
+{
+    Console.WriteLine("Started boiling the water");
+    await WaitingInfo.WaitNumbers(25, "boil the water");
+    Console.WriteLine("Water is boiled");
+}
+
+async Task PrepareCupAsync()
+{
+    Console.WriteLine("Preparing cup for coffee");
+    await WaitingInfo.WaitNumbers(5, "prepare cup", "c");
+    Console.WriteLine("Cup is prepared");
+}
+
+async Task PrepareMilkAsync()
+{
+    Console.WriteLine("Preparing milk");
+    await WaitingInfo.WaitNumbers(5, "prepare milk", "m");
+    Console.WriteLine("Milk is prepared");
+}
+
+await FillWaterAsync();
+Task boiledWater = BoilWaterAsync();
+
+
+await PrepareCupAsync();
+await PrepareMilkAsync();
+await boiledWater;
+
---
+++ WaitingInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CSharpHub
+{
+    public class WaitingInfo
+    {
+        public static async Task WaitDots(int seconds, string waitingFor = "")
+        {
+            Console.Write(waitingFor == "" ? "Waiting..." : $"Waiting for {waitingFor}");
+
+            for (int i = seconds; i > 0; i--)
+            {
+                await Task.Delay(1000);
+                Console.Write(".");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static async Task WaitNumbers(int seconds, string waitingFor = "", string symbol = "")
+        {
+            Console.WriteLine(waitingFor == "" ? "Waiting..." : $"Waiting for {waitingFor}");
+
+            for (int i = seconds; i > 0; i--)
+            {
+                await Task.Delay(1000);
+                Console.Write($"{i}{symbol} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
